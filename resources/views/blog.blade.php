35626778af6cb66012fb64e5b2372661630b74e1
Superred55 <didar5055@gmail.com>
2019-09-09 17:40:16 +0600

    0.1.2

--- resources/views/blog.blade.php
+++ resources/views/blog.blade.php
@@ -1,6 +1,6 @@
 @extends("layouts.master")
 @section("content")
-    <div id="parallax2" class="parallax" style="background-image: url(images/photo3.jpg);">
+    <div id="parallax2" class="parallax" >
         <div class="bg2 parallax-bg" ></div>
         <div class="overlay"></div>
         <div class="parallax-content">
